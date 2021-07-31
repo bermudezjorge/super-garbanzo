5c6226ace81730555c9563899ec38649562b7a2c
bermudezjorge <bermudezjorge1611@gmail.com>
2021-07-30 20:39:17 -0430

    chore: execute tests, ignore

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    "702622333622-55863f2l9vvt6lc9hkkbr8neb2oc3f9r.apps.googleusercontent.com",
-  googleClientSecret: "cMtZSVa_WLE9P2IR-IwvNV8_",
-  mongoURI: `mongodb://numbers:aLr3At2RHGfcI0zl@kai-shard-00-00.cibcf.mongodb.net:27017,kai-shard-00-01.cibcf.mongodb.net:27017,kai-shard-00-02.cibcf.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-5f9rlb-shard-0&authSource=admin&retryWrites=true&w=majority`,
-  cookieKey: "123123123",
-  redisUrl: "redis://127.0.0.1:6379",
-};
