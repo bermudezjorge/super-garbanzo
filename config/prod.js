9c7299c4a9e7a39a9f4eaa18c3013a20e9f530f3
bermudezjorge <bermudezjorge1611@gmail.com>
2021-07-31 12:53:00 -0430

    fix: configs

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: `mongodb://127.0.0.1:27017/blog_ci`,
-  cookieKey: "123123123",
-  redisUrl: "redis://127.0.0.1:6379",
+  cookieKey: proces.env.COOKIE_KEY,
+  mongoURI: process.env.MONGO_URI,
+  redisUrl: process.env.REDIS_URL,
 };
