eba1496c243440cb04dd13ac21ede5d259a1e51f
berjo <76016511+bermudezjorge@users.noreply.github.com>
2021-07-30 20:33:35 -0400

    Delete config directory

--- config/ci.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    "702622333622-55863f2l9vvt6lc9hkkbr8neb2oc3f9r.apps.googleusercontent.com",
-  googleClientSecret: "cMtZSVa_WLE9P2IR-IwvNV8_",
-  mongoURI: `mongodb://127.0.0.1:27017/blog_ci`,
-  cookieKey: "123123123",
-  redisUrl: "redis://127.0.0.1:6379",
-};
